313546329756e2527845f5d34370f01291283f4e
NeewLife <dhtpdud2009@gmail.com>
2022-11-01 22:01:50 +0900

    1101

--- test/js/main.js
+++ test/js/main.js
@@ -1,3 +1,4 @@
+var chatAni = document.querySelector('.main-chat');
 var restAni = document.querySelector(".main-resttime");
 var creditAni = document.querySelector(".main-credits");
 var noticeAni = document.querySelector(".main-noticeBoard");
@@ -15,6 +16,25 @@ noticeAni.addEventListener("animationstart", function(e) {
     noticeAni.style.opacity = "1";
 }, false);
 
-// goChat() {
+console.log(chatAni.style);
+console.log(chatAni.style.animationDuration);
+
+chatAni.addEventListener("click", function(){
+    chatAni.style.removeProperty('animation', 'move1')
+    chatAni.style.animationDuration = "";
+    chatAni.style.animationName = "move1";
+    chatAni.style.animationDuration = "1s";
+    console.log(chatAni.style.animationName);
+    console.log(chatAni.style.animationDuration);
+})
+
+// function goChat() {
+//     chatAni.style.animationName = "move1";
+//     chatAni.style.animationDuration = "1s";
     
+    
+
+//     // setTimeout(function(){
+//     //     window.location.href = './post/Chat.html';
+//     // }, 1800);
 // }
